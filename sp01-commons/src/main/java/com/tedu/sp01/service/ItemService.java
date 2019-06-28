bfcc70055f443e55bd7fc19ff0b2513a5974c571
liulangdefeng <1971191439@qq.com>
2019-06-28 09:38:39 +0800

    11

--- sp01-commons/src/main/java/com/tedu/sp01/service/ItemService.java
+++ sp01-commons/src/main/java/com/tedu/sp01/service/ItemService.java
@@ -7,4 +7,6 @@ import com.tedu.sp01.pojo.Item;
 public interface ItemService {
 	List<Item> getItems(String orderId);
 	void decreaseNumbers(List<Item> list);
+	//11111111111111111111
+	//111111111111asdas
 }
